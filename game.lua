31f7589547297301eb79fdfda250c0c2ff3cf4f1
SaulAcatzin <saulal_@outlook.com>
2015-03-31 09:02:11 -0600

    1B

--- game.lua
+++ game.lua
@@ -1,7 +1,7 @@
 ------------------------------------------------------------
 -- Universidad Tecnológica de San Juan del Río
 -- Saúl Acatzin López Roque
--- 24/Marzo/2015
+-- 31/Marzo/2015
 ------------------------------------------------------------
 --Juego "COPETENCIAS"
 
